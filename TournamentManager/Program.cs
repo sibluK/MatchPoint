2873cf9a72f3828626b686cb3e7b1d912bfc7eb7
sibluK <nedask465@gmail.com>
2025-02-02 21:44:48 +0200

    added breadcrumbs, started player profile layout, changed the mainlayout a bit to fit the breadcrumbs

--- TournamentManager/Program.cs
+++ TournamentManager/Program.cs
@@ -22,6 +22,7 @@ builder.Services.AddScoped<IdentityUserAccessor>();
 builder.Services.AddScoped<IdentityRedirectManager>();
 builder.Services.AddScoped<AuthenticationStateProvider, IdentityRevalidatingAuthenticationStateProvider>();
 builder.Services.AddScoped<PlayerService>();
+builder.Services.AddScoped<MenuService>();
 
 
 builder.Services.AddAuthentication(options =>
