fee4f0a0b785a2d6c102a9a04f8640414f9170ae
sibluK <nedask465@gmail.com>
2025-02-11 12:53:53 +0200

    team page done, just added the teams into a table similar to players page

--- TournamentManager/Services/TeamService.cs
+++ TournamentManager/Services/TeamService.cs
@@ -1,6 +1,20 @@
-﻿namespace TournamentManager.Services;
+﻿using Microsoft.EntityFrameworkCore;
+using TournamentManager.Data;
+using TournamentManager.Models;
+
+namespace TournamentManager.Services;
 
 public class TeamService
 {
-    
+    private readonly ApplicationDbContext _context;
+
+    public TeamService(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<List<Team>> GetAllTeamsAsync()
+    {
+        return await _context.Teams.ToListAsync();
+    }
 }
