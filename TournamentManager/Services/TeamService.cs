49ff95c1979c4392a2fe853b8e1c57acf92dac5b
sibluK <nedask465@gmail.com>
2025-02-22 16:23:19 +0200

    added ability for admins to update teams and players via dialog windows

--- TournamentManager/Services/TeamService.cs
+++ TournamentManager/Services/TeamService.cs
@@ -6,15 +6,53 @@ namespace TournamentManager.Services;
 
 public class TeamService
 {
-    private readonly ApplicationDbContext _context;
+    private readonly ApplicationDbContext _dbContext;
 
-    public TeamService(ApplicationDbContext context)
+    public TeamService(ApplicationDbContext dbContext)
     {
-        _context = context;
+        _dbContext = dbContext;
     }
 
-    public async Task<List<Team>> GetAllTeamsAsync()
+    public async Task<List<Team>> GetAllTeamsAsync(CancellationToken ct)
     {
-        return await _context.Teams.ToListAsync();
+        try
+        {
+            return await _dbContext.Teams.ToListAsync(ct);
+        }
+        catch (OperationCanceledException)
+        {
+            Console.WriteLine("Operation cancelled.");
+            return null!;
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Error fetching team: {ex.Message}");
+            return null!;
+        }
+    }
+
+    public async Task UpdateTeamAsync(Team team, CancellationToken ct)
+    {
+        var existingTeam = await _dbContext.Teams.FirstOrDefaultAsync(x => x.Id == team.Id, ct);
+
+        if (existingTeam is null)
+        {
+            return;
+        }
+        
+        existingTeam.Name = team.Name;
+        existingTeam.Points = team.Points;
+        existingTeam.Ranking = team.Ranking;
+        existingTeam.Region = team.Region;
+        existingTeam.ImagePath = team.ImagePath;
+
+        try
+        {
+            await _dbContext.SaveChangesAsync(ct);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Error updating team: {ex.Message}");
+        }
     }
 }
